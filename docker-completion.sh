0472ec822183c5a0db617a785647ae40a81d9b22
j4ckzh0u <jackzhou2016@qq.com>
2019-11-19 20:55:35 +0800

    Update docker-completion.sh

--- docker-completion.sh
+++ docker-completion.sh
@@ -1,5 +1,7 @@
 yum -y install bash-completion
 
-echo 'source <(docker completion bash)' >> ~/.bashrc
+#echo 'source <(docker completion bash)' >> ~/.bashrc
 
-source ~/.bashrc
+#source ~/.bashrc
+source /usr/share/bash-completion/completions/docker
+source /usr/share/bash-completion/bash_completion
